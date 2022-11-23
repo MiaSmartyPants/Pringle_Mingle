432ef91f865c27490eb6a81567d84a981ccce7be
Mia Dixon <mia.dixon143@gmail.com>
2022-11-22 22:56:50 -0600

    testing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+require('dotenv').config();
 const db = require('./queries')
 const cors = require('cors')
 const path = require('path');
